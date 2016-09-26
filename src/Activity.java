3055909251a4be5d8bc198b1f8afdfb02be8104f
Azudeus <blitzthunder68@yahoo.com>
2016-09-26 23:57:26 +0700

    Fixed strukdat

--- src/Activity.java
+++ src/Activity.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
+
 public class Activity {
 	private String nama;
 	private int[] hari; //Minggu 0 Sabtu 6
 	private int mulai; 
 	private int selesai;
 	private int durasi;
-	private int ruang;
+	private String ruang;
 
-	Activity(String nama, int[] hari, int mulai, int selesai, int durasi, int ruang) {
+	Activity(String nama, ArrayList<Integer> hari, int mulai, int selesai, int durasi, String ruang) {
 		this.nama = nama;
 		this.hari = new int[6];
+		int length = hari.size();
 		for (int i=0;i<7;i++) {
-			this.hari[i] = hari[i];
+			this.hari[i]=0;
+		}
+		for (int i=0;i<length;i++) {
+			this.hari[hari.get(i)] =1;
 		}
 		this.mulai = mulai;
 		this.selesai = selesai;
--- src/Classroom.java
+++ src/Classroom.java
@@ -1,19 +1,41 @@
+import java.util.ArrayList;
+
 public class Classroom {
-	private int nama;
+	private String nama;
 	static int[][] waktu; // value 0 or 1
 
-	Classroom(int nama, int buka, int tutup) {
-		this.nama = 0;
+	Classroom(String nama, int buka, int tutup, ArrayList<Integer> hari) {
+		this.nama = nama;
 		waktu = new int[6][24];
 
 		for (int i=0 ; i < 6 ; i++) {
-			for (int j=0 ; j < 24 ; j++) {
-				if ((j >= buka) && (j <= tutup)) {
-					waktu[i][j] = 1;
-				} else {
+			if (isInDate(buka, hari)) {
+				for (int j=0 ; j < 24 ; j++) {
+					if ((j >= buka) && (j <= tutup)) {
+						waktu[i][j] = 1;
+					} else {
+						waktu[i][j] = 0;
+					}
+				} 
+			} else {
+				for (int j=0 ; j < 24 ; j++) {
 					waktu[i][j] = 0;
 				}
-			} 	
+			}
 		}
+	}
+
+	private boolean isInDate(int date, ArrayList<Integer> arr) {
+		boolean ret = false;
+		int length = arr.size();
+		int i = 0;
+		while ((i<length) && (!ret)) {
+			if (date == arr.get(i)) {
+				ret = true;
+			} else {
+				i++;
+			}
+		}
+		return ret;
 	}
 }
