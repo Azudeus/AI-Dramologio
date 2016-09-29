dc30a39914aa037b5e67de5d348bacfaf84de500
Nikolas Wangsaputra <13514048@std.stei.itb.ac.id>
2016-09-29 14:59:32 +0700

    Updated main page with parser

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title> Login page </title>
+		<title> Oriental </title>
 	</head>
 	
 <?php
@@ -11,6 +11,53 @@
 	
 	<body>
 		
+		<div style="margin-left:30%; width:40%; background-color:#ff0000">
+		<form action="index.php" method="post">
+			<input type="submit" name="genetic" value="Genetic"/>
+			<input type="submit" name="hc" value="Hill Climbing"/>
+			<input type="submit" name="sa" value="Simulated Annealing"/>
+		</form>
+		</div>
+		<br><br>
+		
+		<?php
+			if(isset($_POST["genetic"])){
+				exec("java -cp ".getcwd()."/src genetic",$arrayOfAct);
+				for ($i=0; $i < count($arrayOfAct); $i++){
+					$arrayOfAct[$i] = preg_split("/[\s,]+/", $arrayOfAct[$i]);
+				}
+				for ($i=0; $i < count($arrayOfAct); $i++){
+					for ($j=0; $j < 5; $j++){			
+						echo $arrayOfAct[$i][$j]. " ";
+					}
+					echo "<br>";
+				}
+			}
+			if(isset($_POST["hc"])){
+				exec("java -cp ".getcwd()."/src hc",$arrayOfAct);
+				for ($i=0; $i < count($arrayOfAct); $i++){
+					$arrayOfAct[$i] = preg_split("/[\s,]+/", $arrayOfAct[$i]);
+				}
+				for ($i=0; $i < count($arrayOfAct); $i++){
+					for ($j=0; $j < 5; $j++){			
+						echo $arrayOfAct[$i][$j]. " ";
+					}
+					echo "<br>";
+				}
+			}
+			if(isset($_POST["sa"])){
+				exec("java -cp ".getcwd()."/src sa",$arrayOfAct);
+				for ($i=0; $i < count($arrayOfAct); $i++){
+					$arrayOfAct[$i] = preg_split("/[\s,]+/", $arrayOfAct[$i]);
+				}
+				for ($i=0; $i < count($arrayOfAct); $i++){
+					for ($j=0; $j < 5; $j++){			
+						echo $arrayOfAct[$i][$j]. " ";
+					}
+					echo "<br>";
+				}
+			}
+		?>
 	</body>
 
 </html>
