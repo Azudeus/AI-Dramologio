b0b4429310d6487b20713d69fd98e5997ef75255
Nathan <13514083@std.stei.itb.ac.id>
2016-10-02 02:51:59 +0900

    Add FileReader

---
+++ src/FileReader.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * Created by njruntuwene on 10/2/16.
+ */
+public class FileReader {
+    private Scanner sc;
+
+    public FileReader() {
+        this("test/Testcase.txt");
+    }
+
+    public FileReader(String inputFile) {
+        try{
+            sc = new Scanner(new File(inputFile));
+        } catch (FileNotFoundException e){
+            System.out.println("File not found");
+            System.exit(0);
+        }
+    }
+
+    public ArrayList<Classroom> parseArrayClassroom() {
+        ArrayList<Classroom> classrooms = new ArrayList<>();
+
+        String line = null;
+
+        while ((sc.hasNextLine()) && !Objects.equals("Jadwal", (line != null) ? line.trim() : null)) {
+            line = sc.nextLine();
+            try {
+                classrooms.add(Parser.parseClassroom(line));
+            } catch (Exception e) {
+
+            }
+        }
+        return classrooms;
+    }
+
+    public ArrayList<Activity> parseArrayActivity() {
+        ArrayList<Activity> activities = new ArrayList<>();
+
+        String line = null;
+
+        while ((sc.hasNextLine()) && !Objects.equals("Ruangan", (line != null) ? line.trim() : null)) {
+            line = sc.nextLine();
+            try {
+                activities.add(Parser.parseActivity(line));
+            } catch (Exception e) {
+
+            }
+        }
+
+        return activities;
+    }
+
+}
